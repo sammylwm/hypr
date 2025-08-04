92587584ea1e240d5f2da525b5e21a149789cf90
sammy <lednevdosa@gmail.com>
2025-08-04 16:07:04 +0300

    gcp.fish

--- aliases/gcp.fish
+++ aliases/gcp.fish
@@ -1,3 +1,15 @@
 function gcp
-    git add . && git commit -m "$argv" && git push
+    if test -d $argv[1]
+        set path $argv[1]
+        set msg (string join ' ' $argv[2..-1])
+        pushd $path
+        git add .
+        git commit -m "$msg"
+        git push
+        popd
+    else
+        git add .
+        git commit -m (string join ' ' $argv)
+        git push
+    end
 end
