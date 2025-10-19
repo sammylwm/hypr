cf708aa26681181ab3c614bab55fb6156b4572e4
Sammy <lednevdosa@gmail.com>
2025-10-19 18:07:28 +0300

    install packages

--- install.sh
+++ install.sh
@@ -18,3 +18,23 @@ for dir in "$HOME/hypr/dotfiles"/*; do
 done
 
 hyprctl reload >/dev/null 2>&1
+
+# Список требуемых программ
+packages="caelestia-shell-git caelestia-cli foot"
+
+is_installed() {
+    if pacman -Q "$1" >/dev/null 2>&1; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+for pkg in $packages; do
+    if ! is_installed "$pkg"; then
+        echo "Пакет $pkg не установлен. Устанавливаю через paru..."
+        paru -S --needed --noconfirm "$pkg"
+    else
+        echo "Пакет $pkg уже установлен."
+    fi
+done
